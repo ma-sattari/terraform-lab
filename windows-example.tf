4733e2a31c4068a83474a86f182a1fbfe64f45de
Masoud Sattari <masoud.sattari@argedor.com>
2019-11-08 15:48:34 +0300

    Add windows sample

---
+++ windows-example.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  access_key = "ACCESS_KEY_HERE"
+  secret_key = "SECRET_KEY_HERE"
+  region     = "eu-central-1"
+}
+
+resource "aws_instance" "win-example" {
+  ami           = "ami-066663db63b3aa675"
+  instance_type = "t2.micro"
+  key_name = "key-pair"  ## Existing key pair
+  security_groups = ["${aws_security_group.allow_rdp.name}"]
+
+}
+
+resource "aws_security_group" "allow_rdp" {
+  name        = "allow_rdp"
+  description = "Allow RDP traffic"
+
+
+  ingress {
+
+    from_port   = 3389 #  By default, the windows server listens on TCP port 3389 for RDP
+    to_port     = 3389
+    protocol =   "tcp"
+
+    cidr_blocks =  ["0.0.0.0/0"]  ## Allowed IP addresses
+  }
+}
